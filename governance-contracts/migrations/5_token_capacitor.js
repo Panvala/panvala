8a9f3038abbfd14f5cea677164d9e0b3ef0e2e43
Akua Nti <akua.nti@consensys.net>
2019-06-17 15:28:09 -0400

    Check sender balance in migration

--- governance-contracts/migrations/5_token_capacitor.js
+++ governance-contracts/migrations/5_token_capacitor.js
@@ -4,10 +4,10 @@ const TokenCapacitor = artifacts.require('TokenCapacitor');
 const ParameterStore = artifacts.require('ParameterStore');
 const BasicToken = artifacts.require('BasicToken');
 
-const { abiEncode } = require('../utils');
+const { abiEncode, BN } = require('../utils');
 
 // eslint-disable-next-line func-names
-module.exports = async function (deployer) {
+module.exports = async function (deployer, _, accounts) {
   const parameters = await ParameterStore.deployed();
   console.log(`Deploying TokenCapacitor with ParameterStore ${parameters.address}`);
 
@@ -24,12 +24,18 @@ module.exports = async function (deployer) {
 
   if (charge) {
     console.log(`Charging capacitor with ${initialBalance} tokens`);
-    // transfer
     const token = tokenInfo.deploy
       ? await BasicToken.deployed()
       : await BasicToken.at(tokenInfo.address);
 
     // if the creator has enough balance, then charge
-    await token.transfer(capacitor.address, initialBalance);
+    const [creator] = accounts;
+    const balance = await token.balanceOf(creator);
+    if (balance.gte(new BN(initialBalance))) {
+      await token.transfer(capacitor.address, initialBalance);
+      await capacitor.updateBalances();
+    } else {
+      throw new Error("You don't have enough tokens to charge the capacitor");
+    }
   }
 };
