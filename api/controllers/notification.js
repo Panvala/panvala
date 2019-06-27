e2ded18942d121c5e0bd0fed401284a032261ca5
Isaac Kang <isaac.kang@consensys.net>
2019-06-27 13:19:25 -0400

    add comments

--- api/controllers/notification.js
+++ api/controllers/notification.js
@@ -20,9 +20,12 @@ module.exports = {
     try {
       getAllEvents().then(events => {
         events.map(e => {
+          // print out event name and block.timestamp
           console.log(e.name, e.timestamp);
+          // print out event values for debugging
           Object.keys(e.values).map(arg => {
             let value = e.values[arg];
+            // filter out numerical duplicates, like { 0: '0x1234', voter: '0x1234' }, and the `length` field
             if (numRegex.test(arg) && arg !== 'length') {
               if (value.hasOwnProperty('_hex')) {
                 value = value.toString();
