3eb1999905c925167168606949ef4fdf071ac27b
Isaac Kang <isaac.kang@consensys.net>
2019-07-12 09:28:24 -0400

    add readline & confirm invasive table destruction

--- api/scripts/nukedb.js
+++ api/scripts/nukedb.js
@@ -1,18 +1,34 @@
+const readline = require('readline');
 const { SubmittedBallot, Slate } = require('../models');
 
-function nuke() {
-  Slate.truncate({
-    cascade: true,
-    restartIdentity: true,
-  }).then(() => {
-    SubmittedBallot.truncate({
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`Do you want to truncate the Slate table? [y/N]`, async answer => {
+  if (answer === 'y') {
+    console.log('nuking slate and submittedBallots');
+    await Slate.truncate({
       cascade: true,
       restartIdentity: true,
-    }).then(() => {
-      console.log('NUKED!');
-      process.exit(0);
     });
-  });
-}
+  } else {
+    console.log('Skipping truncation of Slate table');
+  }
 
-nuke();
+  rl.question(`Do you want to truncate the SubmittedBallot table? [y/N]`, async answer => {
+    if (answer === 'y') {
+      console.log('nuking slate and submittedBallots');
+      await SubmittedBallot.truncate({
+        cascade: true,
+        restartIdentity: true,
+      });
+    } else {
+      console.log('Skipping truncation of SubmittedBallot table');
+    }
+
+    rl.close();
+    process.exit(0);
+  });
+});
