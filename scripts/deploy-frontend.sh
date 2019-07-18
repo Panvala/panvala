5479abfd8bf14900d3eba14947a1e33a9cd27f3b
Akua Nti <akua.nti@consensys.net>
2019-07-18 18:41:16 -0400

    Read different API host for staging deploy
    If environment is staging, read variable STAGING_API_HOST when
    deploying frontend.

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -3,7 +3,7 @@
 # deploy frontend
 # deploy-frontend.sh ENVIRONMENT
 # Environment is staging or production
-# Creates service panvala-api-${ENVIRONMENT}
+# Creates service panvala-frontend-${ENVIRONMENT}
 set -u
 set -e
 
@@ -13,9 +13,18 @@ set -e
 # Deploy
 deploy() {
     ENVIRONMENT=$1
-    TAG=$(get_image_tag panvala/api)
+    TAG=$(get_image_tag panvala/frontend)
     REPO="$(get_image_registry)/panvala/frontend"
     APP="panvala-frontend"
+
+    HOST=${API_HOST}
+
+    # HACK: if staging, use a different variable
+    if [ "${ENVIRONMENT}" = "staging" ]
+    then
+       HOST=${STAGING_API_HOST}
+    fi
+
 
     helm upgrade --install \
         --namespace ${ENVIRONMENT} \
@@ -23,7 +32,7 @@ deploy() {
         --set image.tag=${TAG} \
         --set image.repository=${REPO} \
         --set service.type=LoadBalancer \
-        --set apiHost=${API_HOST} \
+        --set apiHost=${HOST} \
         --set nameOverride="${APP}" \
         --set fullnameOverride="${APP}" \
         "${APP}-${ENVIRONMENT}" \
