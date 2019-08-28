7f7dffa31340d33b64a2bbe534ac40736c771b15
Isaac Kang <isaac.kang@consensys.net>
2019-08-28 17:13:35 -0400

    skip integration test (past launchDay)

--- governance-contracts/test/integration.js
+++ governance-contracts/test/integration.js
@@ -1076,7 +1076,9 @@ contract('integration', (accounts) => {
   describe('capacitor releases', () => {
     const [creator, recommender, partner] = accounts;
 
-    it('deployment - should allow withdrawal of the correct number of tokens on Nov 1', async () => {
+    // Skipping this test because utils.evm.goTo(launchDay) does not succeed (because we've already launched & ganache-cli can only go forward in time),
+    // causing issues re: expected unlocked/locked balances, due to unexpected `now` during capacitor.updateBalances().
+    it.skip('deployment - should allow withdrawal of the correct number of tokens on Nov 1', async () => {
       const initialTokens = 49906303;
       // const initialBalance = new BN(toPanBase(initialTokens));
       // const scale = new BN(1e12);
