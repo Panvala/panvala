b41fdb3d9b30af082aa1b9383b1812e9c2ed7746
Peter Yinusa <peter.yinusa@gmail.com>
2019-08-29 10:46:57 +0100

    add missing config

---
+++ e2e-tests/src/features/config/envConfig.js
@@ -0,0 +1,11 @@
+import config from '../../../config/Environment.json';
+
+export const SELENIUM_BROWSER = config.browser;
+export const PANVALA_APP_URL = config.environment.web.url;
+export const PANVALA_API_URL = config.environment.api.url;
+export const METAMASK_CRX = config.metamask.crx;
+export const METAMASK_URL = config.metamask.url;
+export const METAMASK_PASSWORD = config.metamask.password;
+export const METAMASK_SEED = config.metamask.seed;
+export const METAMASK_NETWORK_NAME = config.metamask.network.name;
+export const METAMASK_NETWORK_URL = config.metamask.network.url;
---
+++ e2e-tests/src/features/config/testConfig.js
@@ -0,0 +1,3 @@
+import config from './testConfig.json';
+
+export const AWARD_ADDRESS = config.awardAddress;
