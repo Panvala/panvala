e349fa2e3edb1fac2b0be483f4e367bbf1d9aa40
Isaac Kang <isaac.kang@consensys.net>
2019-09-11 15:51:56 -0400

    change donate button to input

--- website/build/DonateButton.js
+++ website/build/DonateButton.js
@@ -4,8 +4,12 @@ function DonateButton(_ref) {
   var {
     handleClick
   } = _ref;
-  return React.createElement("div", null, React.createElement("button", {
+  return React.createElement("div", null, React.createElement("input", {
+    type: "submit",
+    name: "submit",
     onClick: handleClick,
-    className: "f6 link dim bn br-pill pv3 ph4 white bg-teal fw7 mt4"
-  }, "Donate!"));
+    onSubmit: handleClick,
+    className: "f6 link dim bn br-pill pv3 ph4 white bg-teal fw7 mt4",
+    value: "Donate"
+  }));
 }
--- website/src/DonateButton.js
+++ website/src/DonateButton.js
@@ -1,14 +1,16 @@
-'use strict'
+'use strict';
 
 function DonateButton({ handleClick }) {
   return (
     <div>
-      <button
+      <input
+        type="submit"
+        name="submit"
         onClick={handleClick}
+        onSubmit={handleClick}
         className="f6 link dim bn br-pill pv3 ph4 white bg-teal fw7 mt4"
-      >
-        Donate!
-      </button>
+        value="Donate"
+      />
     </div>
   );
 }
