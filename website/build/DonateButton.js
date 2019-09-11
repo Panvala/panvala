61f7ef5303b5819b56e1a2fafccf4ee3ddea2e71
Isaac Kang <isaac.kang@consensys.net>
2019-09-11 15:55:49 -0400

    rm extraneous onSubmit handler

--- website/build/DonateButton.js
+++ website/build/DonateButton.js
@@ -8,7 +8,6 @@ function DonateButton(_ref) {
     type: "submit",
     name: "submit",
     onClick: handleClick,
-    onSubmit: handleClick,
     className: "f6 link dim bn br-pill pv3 ph4 white bg-teal fw7 mt4",
     value: "Donate"
   }));
--- website/src/DonateButton.js
+++ website/src/DonateButton.js
@@ -7,7 +7,6 @@ function DonateButton({ handleClick }) {
         type="submit"
         name="submit"
         onClick={handleClick}
-        onSubmit={handleClick}
         className="f6 link dim bn br-pill pv3 ph4 white bg-teal fw7 mt4"
         value="Donate"
       />
