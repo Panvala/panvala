06e1644e5d0ab87c57b0b2c4cb812c5e73b81762
Akua Nti <akua.nti@consensys.net>
2019-11-07 17:17:53 -0500

    Add stake withdrawal and donation to finalize script
    This currently only handles grant slates.

--- api/scripts/finalize.ts
+++ api/scripts/finalize.ts
@@ -2,6 +2,7 @@ import * as ethers from 'ethers';
 import * as readline from 'readline';
 import { getProposalsForRequests } from '../src/utils/requests';
 import { sequelize } from '../src/models';
+import { bigNumberify } from 'ethers/utils';
 
 const { voting } = require('../src/utils');
 const { ContestStatus } = voting;
@@ -29,7 +30,8 @@ function printFunctionSupport(intended = 'finalizeContest', alternative = 'count
 
 async function firstRoundVote(epochNumber, resource, gkFuncs) {
   if (gkFuncs.hasOwnProperty('finalizeContest')) {
-    await gkFuncs.finalizeContest(epochNumber, resource);
+    const tx = await gkFuncs.finalizeContest(epochNumber, resource);
+    console.log(tx.hash);
   } else {
     printFunctionSupport();
     await gkFuncs.countVotes(epochNumber, resource);
@@ -50,15 +52,13 @@ async function finalize(gatekeeper, epochNumber, resource, index) {
   // single slate
   if (status === ContestStatus.NoContest) {
     console.log(`No challenger for resource ${resource} -- automatically finalizing`);
-    const receipt = await firstRoundVote(epochNumber, resource, gkFuncs);
-    console.log(receipt);
+    await firstRoundVote(epochNumber, resource, gkFuncs);
   }
 
   // active contest
   if (status === ContestStatus.Active) {
     console.log('Counting votes for epochNumber, resource, status', epochNumber, resource, status);
-    const receipt = await firstRoundVote(epochNumber, resource, gkFuncs);
-    console.log(receipt);
+    await firstRoundVote(epochNumber, resource, gkFuncs);
   }
 
   // contest already finalized
@@ -77,9 +77,8 @@ async function disburseGrants(
   capacitor,
   epochNumber
 ) {
-  const gk = gatekeeper.functions;
   const resource = capacitor.address;
-  const status = (await gk.contestStatus(epochNumber, resource)).toString();
+  const status = (await gatekeeper.contestStatus(epochNumber, resource)).toString();
   console.log('status', status);
 
   if (status !== ContestStatus.Finalized) {
@@ -87,11 +86,11 @@ async function disburseGrants(
   }
 
   // get requests corresponding to the winning slate
-  const winner = await gk.getWinningSlate(epochNumber, resource);
+  const winner = await gatekeeper.getWinningSlate(epochNumber, resource);
   console.log('Winning slate: ', winner.toString());
 
   // get requests
-  const requestIDs = (await gk.slateRequests(winner)).map(r => r.toString());
+  const requestIDs = (await gatekeeper.slateRequests(winner)).map(r => r.toString());
   console.log('REQUESTS', requestIDs);
 
   const proposalIDs = await getProposalsForRequests(resource, requestIDs).then(proposals =>
@@ -129,6 +128,7 @@ async function disburseGrants(
   await sequenceNonces(provider, wallet, tasks);
 }
 
+// TODO: move to panvala-utils
 // Execute a set of functions in sequence passing the sequential nonces
 async function sequenceNonces(provider, wallet: ethers.Wallet, asyncFuncs) {
   // https://github.com/ethers-io/ethers.js/issues/435#issuecomment-467284296
@@ -143,6 +143,35 @@ async function sequenceNonces(provider, wallet: ethers.Wallet, asyncFuncs) {
 
     await fn(nonce);
   }
+}
+
+async function getWinningSlate(gatekeeper, epochNumber, resource) {
+  const status = (await gatekeeper.contestStatus(epochNumber, resource)).toString();
+  if (status !== ContestStatus.Finalized) {
+    console.log('Contest has not been finalized.');
+    return null;
+  }
+
+  // Enrich slate with slateID field
+  const winningSlate = await gatekeeper.getWinningSlate(epochNumber, resource).then(slateID =>
+    gatekeeper.slates(slateID).then(slate => {
+      return { ...slate, slateID };
+    })
+  );
+  // console.log('Winning slate: ', winningSlate);
+  return winningSlate;
+}
+
+async function askUser(question: string, handleAnswer) {
+  return new Promise(resolve => {
+    console.log();
+    rl.question(question, async _answer => {
+      // Do it
+      await handleAnswer(_answer);
+
+      resolve();
+    });
+  });
 }
 
 async function run() {
@@ -164,7 +193,7 @@ async function run() {
 
   // Finalize contests
   try {
-    console.log('Finalizing ballots for all categories...');
+    console.log('> Finalizing ballots for all categories...');
     const finalizeAllCategories = resources.map((resource, index) =>
       finalize(gatekeeper, epochNumber, resource, index)
     );
@@ -174,30 +203,84 @@ async function run() {
     console.error(error);
   }
 
+  // Get winning slates for each resource (null if not finalized)
+  const winningSlates = await Promise.all(
+    resources.map(resource => getWinningSlate(gatekeeper, epochNumber, resource))
+  );
+
+  const writeTokenCapacitor = tokenCapacitor.connect(wallet);
+
   // Execute proposals
-  try {
-    await new Promise(resolve => {
-      console.log();
-      rl.question('Do you want to execute proposals? [y/N]', async answer => {
-        if (answer === 'y') {
-          console.log('Disbursing grants');
-          const writeTokenCapacitor = tokenCapacitor.connect(wallet);
-          await disburseGrants(wallet, provider, gatekeeper, writeTokenCapacitor, epochNumber);
-
-          // TODO: call setValue
-        } else {
-          console.log('Not executing proposals');
+  await askUser('Do you want to execute proposals? [y/N]', async answer => {
+    if (answer === 'y') {
+      console.log('> Disbursing grants');
+      await disburseGrants(wallet, provider, gatekeeper, writeTokenCapacitor, epochNumber).catch(
+        error => {
+          console.log('Problem disbursing grants', error);
         }
+      );
 
-        resolve();
+      // TODO: handle governance slate
+    } else {
+      console.log('> Not executing proposals');
+    }
+  }).catch(error => {
+    console.log(error);
+  });
+
+  const [grantWinner] = winningSlates;
+
+  if (grantWinner != null) {
+    console.log();
+    // console.log('GRANT', grantWinner);
+    const { slateID, stake, staker, resource } = grantWinner;
+
+    if (stake.gt(bigNumberify(0))) {
+      console.log(`Slate ${slateID} has a stake of ${stake.toString()}`);
+
+      if (wallet.address === staker) {
+        console.log('You are the original staker!');
+        await askUser('Do you want to withdraw this stake? [y/N]', async answer => {
+          if (answer === 'y') {
+            console.log(`> Withdrawing stake for slate ${slateID}`);
+            const tx = await gatekeeper.withdrawStake(slateID);
+            console.log(tx.hash);
+            await tx.wait();
+          } else {
+            console.log(`> Not withdrawing stake for slate ${slateID}`);
+          }
+        }).catch(error => {
+          console.log('Problem withdrawing stake');
+          console.log(error);
+        });
+      } else {
+        // console.log('You are not the original staker');
+      }
+    } else {
+      console.log('No stake left');
+    }
+
+    const { stakedSlates } = await gatekeeper.contestDetails(epochNumber, resource);
+    // Donate challenger stakes if there are any
+    if (stakedSlates.length > 1) {
+      console.log(`There are challenger slates`);
+      await askUser('Do you want to donate challenger stakes? [y/N]', async answer => {
+        if (answer == 'y') {
+          console.log('> Donating challenger stakes');
+          const tx = await gatekeeper.donateChallengerStakes(
+            epochNumber,
+            resource,
+            0,
+            stakedSlates.length
+          );
+          console.log(tx.hash);
+          await tx.wait();
+        } else {
+          console.log('> Not donating challenger stakes');
+        }
       });
-    });
-  } catch (error) {
-    console.error(error);
+    }
   }
-
-  // TODO: donate challenger stakes
-  // TODO: withdraw stake
 
   rl.close();
   process.exit(0);
