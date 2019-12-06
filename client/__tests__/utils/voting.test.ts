f88251256ec959ed4bd539c0061ecf096cd68fbe
Isaac Kang <isaac.kang@consensys.net>
2019-12-06 13:57:39 -0500

    print salt in strange, failing test

--- client/__tests__/utils/voting.test.ts
+++ client/__tests__/utils/voting.test.ts
@@ -1,9 +1,9 @@
 import { randomSalt } from '../../utils/voting';
 
-
 describe('Voting', () => {
   test('should create a random salt', () => {
     const salt = randomSalt();
+    console.log('salt.toHexString():', salt.toHexString());
 
     // length = 2 * 32 bytes + `0x`
     expect(salt.toHexString().length).toEqual(66);
