9df61a824e196718b854181d544924037402c8a1
Niran Babalola <niran@niran.org>
2020-02-19 14:48:09 -0600

    Revert "Add an ETHDenver call to action so people know how to get their XP"
    This reverts commit 3a64a9289af94cc668be861260d580363ee315f7.

--- gatsby-site/src/pages/join/index.tsx
+++ gatsby-site/src/pages/join/index.tsx
@@ -28,36 +28,6 @@ const Join = () => {
         </div>
       </section>
 
-      <Box
-        py={['1rem', '2rem']}
-        px={['1rem', '3.5rem']}
-        mt={['-60px', '-120px', '-120px', '-100px']}
-        mx={['3rem', '6rem', '9rem']}
-        boxShadow="0px 0px 40px rgba(0, 0, 0, 0.2)"
-        borderRadius="12px"
-        position="relative"
-        bg="white"
-        zIndex={800}
-      >
-        <Box flex justifyContent="space-between" alignItems="center">
-          <Box>
-            At <a href="https://www.ethdenver.com/">ETHDenver</a>? Once you've voted, send an email to the Caucus to claim
-            your BuffiDAO XP!
-          </Box>
-          <Box flex column alignItems="flex-end">
-            <Box textAlign="right" fontSize={['1rem', '1.1rem', '1.3rem']} bold color="blues.medium">
-              <a
-                href="mailto:caucus@panvala.com?subject=Claiming%20BuffiDAO%20XP"
-                className="link b dim blue"
-                target="_blank"
-              >
-                <Button p={3} mr={3} text="Claim Your XP" bg="#F5F6F9" color="black" />
-              </a>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
-
       <Box className="bottom-clip-down relative z-3 pv4" bg="white">
         <section className="flex flex-wrap">
           <Box className="w-50-ns w-100" bold fontSize={4} textAlign="center">
@@ -110,7 +80,7 @@ const Join = () => {
           </Box>
         </section>
       </Box>
-
+      
       <Box mt="-5vw" className="pv6-ns pv4">
         <section className="flex flex-wrap">
           <Box className="w-50-ns w-100" bold fontSize={4} textAlign="center">
@@ -135,7 +105,7 @@ const Join = () => {
         </section>
       </Box>
 
-
+      
     </Layout>
   );
 };
