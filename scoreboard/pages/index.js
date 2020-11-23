2a496f6878f4d0295869ffcda1577a64d09764b0
Niran Babalola <niran@niran.org>
2020-11-23 03:05:18 +0000

    Add charts to the overview page

--- scoreboard/pages/index.js
+++ scoreboard/pages/index.js
@@ -1,7 +1,33 @@
 import { makeStyles, Box, Button, Grid, Paper, Typography } from '@material-ui/core';
+import { BarChart, Bar, Legend, LineChart, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 import BaseLayout from "../layout";
 
+const quarterlyMetrics = [{
+  date: '4/7/2020',
+  leagueValue: 37650,
+  fullValue: 49312.56,
+  leagueDonations: 2905.47,
+  fullDonations: 9978.74,
+  leagueMultiplier: 12.96,
+  fullMultiplier: 4.94,
+}, {
+  date: '7/3/2020',
+  leagueValue: 50870.73,
+  fullValue: 67827.61,
+  leagueDonations: 9950.78,
+  fullDonations: 12680.81,
+  leagueMultiplier: 5.11,
+  fullMultiplier: 5.35,
+}, {
+  date: '10/2/2020',
+  leagueValue: 151450.49,
+  fullValue: 201933.90,
+  leagueDonations: 14757.34,
+  fullDonations: 17490.25,
+  leagueMultiplier: 10.26,
+  fullMultiplier: 11.55,
+}];
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -59,12 +85,36 @@ export default function Index() {
         </Grid>
         <Grid item sm={12} md={6}>
           <Paper className={classes.gridItemPaper}>
-            <Typography component="h1" variant="h4" gutterBottom>Quarterly Value Allocated</Typography> 
+            <Typography component="h1" variant="h4" gutterBottom>Quarterly Value Allocated</Typography>
+            <div style={{ width: '100%', height: 300 }}>
+              <ResponsiveContainer>
+                <BarChart data={quarterlyMetrics}>
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="leagueValue" name="Panvala League" fill="#2138b7" />
+                  <Bar dataKey="fullValue" name="Full Treasury" fill="#46b0aa" />
+                </BarChart> 
+              </ResponsiveContainer>
+            </div>
           </Paper>
         </Grid>
         <Grid item sm={12} md={6}>
           <Paper className={classes.gridItemPaper}>
-            <Typography component="h1" variant="h4" gutterBottom>Quarterly Donations</Typography> 
+            <Typography component="h1" variant="h4" gutterBottom>Quarterly Donations</Typography>
+            <div style={{ width: '100%', height: 300 }}>
+              <ResponsiveContainer>
+                <BarChart data={quarterlyMetrics}>
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="leagueDonations" name="Panvala League" fill="#2138b7" />
+                  <Bar dataKey="fullDonations" name="Full Treasury" fill="#46b0aa" />
+                </BarChart> 
+              </ResponsiveContainer>
+            </div>
           </Paper>
         </Grid>
         <Grid item sm={12} md={6}>
@@ -74,7 +124,19 @@ export default function Index() {
         </Grid>
         <Grid item sm={12} md={6}>
           <Paper className={classes.gridItemPaper}>
-            <Typography component="h1" variant="h4" gutterBottom>Matching Multipliers</Typography> 
+            <Typography component="h1" variant="h4" gutterBottom>Matching Multipliers</Typography>
+            <div style={{ width: '100%', height: 300 }}>
+              <ResponsiveContainer>
+                <LineChart data={quarterlyMetrics}>
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Line type="monotone" dataKey="leagueMultiplier" name="Panvala League" stroke="#2138b7" />
+                  <Line type="monotone" dataKey="fullMultiplier" name="Full Treasury" stroke="#46b0aa" />
+                </LineChart> 
+              </ResponsiveContainer>
+            </div> 
           </Paper>
         </Grid>
         <Grid item sm={12}>
