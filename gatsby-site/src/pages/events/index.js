7a1755ba11e17927fdcd30042b717d4148fb440f
Niran Babalola <niran@niran.org>
2020-11-30 01:15:47 +0000

    Add time zone info to events page

--- gatsby-site/src/pages/events/index.js
+++ gatsby-site/src/pages/events/index.js
@@ -35,7 +35,7 @@ const Join = () => {
         <section className="w-70-ns w-100 ph4 mb4">
           <h1 className="tc">Panvala Events</h1>
           <p className="tc ma0 f6 lh-text mb3">
-            This calendar includes Panvala-wide events and events supported by Panvala's <a href="https://handbook.panvala.com/governance/activities-fund">Activities Fund</a>. You can earn PAN by attending most of these events!
+            This calendar includes Panvala-wide events and events supported by Panvala's <a href="https://handbook.panvala.com/governance/activities-fund">Activities Fund</a>. You can earn PAN by attending most of these events! All times are U.S. Eastern time.
           </p>
           <div className="eca-app-container" data-widgetuuid="03dbe142-b422-445d-84c4-da3efe3c0aae"></div>
         </section>
@@ -43,7 +43,7 @@ const Join = () => {
         <section className="w-70-ns w-100 ph4 mb4">
           <h1 className="tc">Panvala League Events</h1>
           <p className="tc ma0 f6 lh-text mb3">
-            This calendar includes events submitted by each Panvala League community.
+            This calendar includes events submitted by each Panvala League community. All times are U.S. Eastern time.
           </p>
           <div className="eca-app-container" data-widgetuuid="082eff3d-2688-4677-a1f7-d4aa18b7a644"></div>
         </section>
