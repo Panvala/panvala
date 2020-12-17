fc71e2704ad51f9e1a4b821f200a4cc6801ab3d8
Niran Babalola <niran@niran.org>
2020-12-17 00:54:37 +0000

    Add more information to each community's scoreboard page

--- scoreboard/pages/[slug].js
+++ scoreboard/pages/[slug].js
@@ -5,7 +5,7 @@ import { Legend, LineChart, Line, ReferenceDot, ResponsiveContainer, Tooltip, XA
 
 import { communitiesBySlug } from '../communities';
 import BaseLayout from "../layout";
-import { getCommunitySubsidyChartData } from '../lib/calculations';
+import { getCommunitySubsidyChartData, parseCommaFloat, parsePercent } from '../lib/calculations';
 import { getSpreadsheetData } from '../lib/static';
 
 export async function getStaticProps(content) {
@@ -23,6 +23,14 @@ const useStyles = makeStyles((theme) => ({
   button: {
     color: "white",
   },
+  fullyStaked: {
+    fontWeight: "bold",
+    color: "green",
+  },
+  notFullyStaked: {
+    fontWeight: "bold",
+    color: "red",
+  },
   gridItemPaper: {
     minHeight: 300,
     padding: theme.spacing(4),
@@ -34,14 +42,14 @@ export default function Community({ scoreboard, totals }) {
   const { slug } = useRouter().query;
   const communityInfo = communitiesBySlug[slug];
   const { line, dot } = getCommunitySubsidyChartData(communityInfo.name, scoreboard, totals);
+  const communityRow = scoreboard[communityInfo.name];
+  const fullyStaked = parsePercent(communityRow['Utilization (Share of QF / Capacity)']) < 100;
 
   return (
     <BaseLayout>
       <Grid container spacing={4}>
         <Grid item xs={12}>
-          <Paper className={classes.gridItemPaper}>
-            <Typography variant="h1" gutterBottom>{communityInfo.name}</Typography>
-          </Paper>
+          <Typography variant="h1" gutterBottom>{communityInfo.name}</Typography>
         </Grid>
         <Grid item xs={12}>
           <Paper className={classes.gridItemPaper}>
@@ -65,6 +73,37 @@ export default function Community({ scoreboard, totals }) {
               The X-axis is the amount of PAN staked by each community. The left Y-axis is the matching PAN each community
               can earn this quarter. The right Y-axis is percent yield on the staked tokens.
             </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.gridItemPaper}>
+            <Grid container spacing={4}>
+              <Grid item xs={12} md={4}>
+                <Typography component="h2" variant="h6">Donations</Typography>
+                <Typography variant="body1">{communityRow['PAN Donated']} PAN</Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography component="h2" variant="h6">Staked PAN</Typography>
+                <Typography variant="body1">{communityRow['Staked Tokens']} PAN</Typography>
+                <Typography variant="body1">{fullyStaked ? 
+                  <span className={classes.fullyStaked}>FULLY STAKED</span> : 
+                  <span className={classes.notFullyStaked}>NOT FULLY STAKED</span>
+                }</Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography component="h2" variant="h6">Matching PAN</Typography>
+                <Typography variant="body1">{communityRow['Estimated Subsidy\n(PAN)']} PAN</Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography component="h2" variant="h6">Total Funding</Typography>
+                <Typography variant="body1">{communityRow['Estimated Funding\n(PAN)']} PAN</Typography>
+                <Typography variant="body1">{communityRow['Estimated Multiplier']}</Typography>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Typography component="h2" variant="h6">Staking Yield</Typography>
+                <Typography variant="body1">{dot.yield}% APY</Typography>
+              </Grid>
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
